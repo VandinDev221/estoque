113d448a4e24a131e8585aa1d2f2a06bc0703ed4
VANDERSON CARLOS ANDRADE LINDOSO <vandersongplay@gmail.com>
2025-03-20 23:40:03 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstoqueController;
 
 Route::get('/', function () {
-    return view('resources/views/estoque/index.blade.php');
+    return view('estoque.index');
 });
 
 Route::get('/estoque', [EstoqueController::class, 'index'])->name('estoque.index');
